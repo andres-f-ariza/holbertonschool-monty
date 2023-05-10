1efbb54d301f5492f71785f3380b50280cf1bc9f
andres-f-ariza <arizaandres8@gmail.com>
2023-05-10 13:29:56 -0700

    fix:opcodespush,pall.compilation1

--- monty.c
+++ monty.c
@@ -38,7 +38,7 @@ void push(stack_t **stack, unsigned int line_number)
 	*stack = new_node;
 }
 
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack, _attribute_((unused)) unsigned int line_number)
 {
 	stack_t *current = *stack;
 
