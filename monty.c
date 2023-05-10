edd0ad16f19aa7ae9abd17d8e0067a34b83d2afb
andres-f-ariza <arizaandres8@gmail.com>
2023-05-10 13:11:37 -0700

    feat:opcodespush,pall

---
+++ monty.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+/* Global variable */
+stack_t *stack = NULL;
+
+/* Function implementations */
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *arg;
+	int value;
+	stack_t *new_node;
+
+	arg = strtok(NULL, " \t\n");
+
+	if (!arg)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	value = atoi(arg);
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = new_node;
+
+	*stack = new_node;
+}
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+
