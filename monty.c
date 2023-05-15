b2c4ec62376ad55ec532e404f6f73248fc5c86f3
root <jorgefernandoochoa1@hotmail.com>
2023-05-15 08:57:49 -0700

    pint

--- monty.c
+++ monty.c
@@ -53,7 +53,7 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	if (!*stack || !stack)
 	{
-		dprintf(STDERR_FILENO, "L%i: can't print, stack empty\n", line_number);
+		dprintf(STDERR_FILENO, "L%i: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%i\n", (*stack)->n);
