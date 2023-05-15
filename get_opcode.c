6f731c2d5393dbe0680388ab64f8c668e4b67766
root <jorgefernandoochoa1@hotmail.com>
2023-05-15 09:55:00 -0700

    handle

--- get_opcode.c
+++ get_opcode.c
@@ -6,7 +6,7 @@
  * Return: Pointer to opcode function
  */
 
-void (*getopcode(char *opcode))(stack_t **, unsigned int)
+void (*getopcode(void))(stack_t **, unsigned int)
 {
 	int i = 0;
 	instruction_t made_opcodes[] = {
@@ -22,5 +22,4 @@ void (*getopcode(char *opcode))(stack_t **, unsigned int)
 			return (made_opcodes[i].f);
 		i++;
 	}
-	return (NULL);
 }
