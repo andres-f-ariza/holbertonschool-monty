f16d587b01f7c4d76f0b6573374199916ccbd3d7
andres-f-ariza <arizaandres8@gmail.com>
2023-05-15 19:39:03 -0700

    fix:outputpush2

--- monty.c
+++ monty.c
@@ -6,47 +6,55 @@
 stack_t *stack = NULL;
 
 /* Function implementations */
-void push(stack_t **stack, unsigned int line_number)
+
+void op_push(stack_t **stack, unsigned int line_number)
 {
-	char *arg;
-	int value;
-	stack_t *new_node;
+	int value_to_add = 0, isint;
+	char *value;
 
-	arg = strtok(NULL, " \t\n");
-
-	if (!arg)
+	value = strtok(NULL, " ");
+	isint = string_is_int(value);
+	if (isint == 0)
+	{
+		value_to_add = atoi(value);
+		add_dnodeint(stack, value_to_add);
+	}
+	else
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		free_dlistint(*stack);
 		exit(EXIT_FAILURE);
 	}
-
-	value = atoi(arg);
-	new_node = malloc(sizeof(stack_t));
-	if (!new_node)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	new_node->n = value;
-	new_node->prev = NULL;
-	new_node->next = *stack;
-
-	if (*stack)
-		(*stack)->prev = new_node;
-
-	*stack = new_node;
 }
+
+/**
+ * op_pall - pall action for monty
+ * @stack: pointer to pointer of head's stack
+ * @line_number: file's line number
+ */
 
 void pall(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 {
-	stack_t *current = *stack;
+	if (stack)
+		print_dlistint(*stack);
+}
 
-	while (current)
+/**
+ * op_pint - prints the value at the top of the stack, followed by a new line.
+ * @stack: pointer to pointer of head's stack
+ * @line_number: file's line number
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !*stack)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free_dlistint(*stack);
+		exit(EXIT_FAILURE);
 	}
+	else
+		printf("%d\n", (*stack)->n);
 }
 
 void pint(stack_t **stack, unsigned int line_number)
